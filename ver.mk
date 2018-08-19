b4ee0c3256ef2febc5b5ab7ee17a75406779faff
Hugo Saporetti Junior <yorevs@gmail.com>
2018-08-19 03:33:45 -0300

    Fix the version make

--- ver.mk
+++ ver.mk
@@ -8,55 +8,82 @@
 # Access: https://github.com/yorevs/ccompass
 # Copyright (C) 2018 Hugo Saporetti Junior
 # -----------------------------------------------------------------------------
+# Targets:
+#
+# [version]
+#    Increment if any object file changes.
+#
+# [major]
+#    Increment the Major number.
+#
+# [minor]
+#    Increment the Minor number.
+# 
+# [revision]
+#    Increment the Revision number.
+#
+# [buildnum]
+#    Increment the build number.
 
 # Version control file.
-BUILD_NUM_FILE   := VERSION
+VERSION_FILE     := VERSION
 
 # If the version file does not exist. Create it and assign version 0.0.1 .
-ifneq "$(wildcard $(BUILD_NUM_FILE) )" ""
-VERSION          ?= $(MAJOR).$(MINOR).$(REVISION)
+ifneq "$(wildcard $(VERSION_FILE) )" ""
+VERSION          ?= $(MAJOR).$(MINOR).$(REVISION).$(BUILDNUM)
 else
-VERSION          ?= 0.0.1
+VERSION          ?= 0.1.0.001
 endif
 
 # Version parts.
-MAJOR            ?= $(shell head -1 $(BUILD_NUM_FILE) | cut -d '.' -f 1)
-MINOR            ?= $(shell head -1 $(BUILD_NUM_FILE) | cut -d '.' -f 2)
-REVISION         ?= $(shell head -1 $(BUILD_NUM_FILE) | cut -d '.' -f 3)
+MAJOR            ?= $(shell head -1 $(VERSION_FILE) | cut -d '.' -f 1)
+MINOR            ?= $(shell head -1 $(VERSION_FILE) | cut -d '.' -f 2)
+REVISION         ?= $(shell head -1 $(VERSION_FILE) | cut -d '.' -f 3)
+BUILDNUM         ?= $(shell head -1 $(VERSION_FILE) | cut -d '.' -f 4)
 BUILD_DATE       := $(shell date +'%d-%m-%Y')
 
 
-# Create the initial BUILD_NUM_FILE.
-$(BUILD_NUM_FILE): $(C_OBJS)
-	$(VERBOSE)if [ ! -f $(BUILD_NUM_FILE) ]; then \
-		echo "0.0.0" > $(BUILD_NUM_FILE); \
+# Create the initial VERSION_FILE.
+$(VERSION_FILE): $(C_OBJS)
+	$(VERBOSE)if [ ! -f $(VERSION_FILE) ]; then \
+		echo "0.1.0.000" > $(VERSION_FILE); \
 	fi;
 
-
 # Version. Increment if any object file changes.
-version: $(BUILD_NUM_FILE)
-	$(MAKE) incr-revision
-	@echo "### Last modified: $(BUILD_DATE)" >> $(BUILD_NUM_FILE)
-	@echo "### Automatically generated. Do not edit !" >>$(BUILD_NUM_FILE)
-
+version: $(VERSION_FILE)
+	$(MAKE) revision
+	@echo "### Last modified: $(BUILD_DATE)" >> $(VERSION_FILE)
+	@echo "### Automatically generated. Do not edit !" >>$(VERSION_FILE)
 
 # Increment the Major number.
-incr-major: $(BUILD_NUM_FILE)
+major: $(VERSION_FILE)
 	@echo '[$@] Incrementing major number'
-	@echo "$$(($(MAJOR) + 1)).0.0" > $(BUILD_NUM_FILE)
+	@echo "$$(($(MAJOR) + 1)).0.0.000" > $(VERSION_FILE)
+	@make ver
 
 # Increment the Minor number.
-incr-minor: $(BUILD_NUM_FILE)
+minor: $(VERSION_FILE)
 	@echo '[$@] Incrementing minor number'
-	@echo "$(MAJOR).$$(($(MINOR) + 1)).0" > $(BUILD_NUM_FILE)
+	@echo "$(MAJOR).$$(($(MINOR) + 1)).0.000" > $(VERSION_FILE)
+	@make ver
 
 # Increment the Revision number.
-incr-revision: $(BUILD_NUM_FILE)
+revision: $(VERSION_FILE)
 	@echo '[$@] Incrementing revision number'
-	@echo "$(MAJOR).$(MINOR).$$(($(REVISION) + 1))" > $(BUILD_NUM_FILE)
+	@echo "$(MAJOR).$(MINOR).$$(($(REVISION) + 1)).000" > $(VERSION_FILE)
+	@make ver
 
+# Increment the build number.
+buildnum: $(VERSION_FILE)
+	@echo '[$@] Incrementing build number'
+	@echo "$(MAJOR).$(MINOR).$(REVISION).$(shell printf '%03d' $$(($(BUILDNUM) + 1)))" > $(VERSION_FILE)
+	@make ver
+
+# Check the current version.
+ver:
+	@echo '[$@] Current version is: $(shell head -1 $(VERSION_FILE))'
 
 # ---------------------------------------------------------------------------- 
 ### PHONY targets.
-.PHONY: version incr-major incr-minor incr-revision
+.PHONY: version major minor revision buildnum ver
 
